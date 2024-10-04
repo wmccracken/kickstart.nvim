e10a14bd9f5ab543cd32793a57f8ba6658e83d47
Will McCracken <will@wgmamm.com>
2024-10-04 13:42:32 -0400

    add tmux plugin

--- init.lua
+++ init.lua
@@ -927,6 +927,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.tmux',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
